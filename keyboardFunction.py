b39206325dd13ed21f93c6bfef381b97a7b8d30c
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-11-06 21:27:12 +0700

    Making keyboardFunction.py and screenshot.png
    This file used for auto typing in keyboard and taking screenshot automatically

---
+++ keyboardFunction.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+time.sleep(3)
+pyautogui.write("Hello World")
+pyautogui.press("enter")
+pyautogui.write("Hello World 2")
+
+pyautogui.screenshot("screenshot.png")Hello World
+Hello World 2
