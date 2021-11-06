f07745ddc0b62d5ed488cc943c723f38dbebce31
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-11-06 12:39:10 +0700

    Making New File POMousePosition.py
    This file will checking the mouse position by x,y coordinates

--- POMousePosition.py
+++ POMousePosition.py
@@ -1,13 +1,12 @@
 import pyautogui
 import sys
 
-print('Press Ctrl-C to quit.')
+print('Press Ctrl + C to Quit')
 try:
     while True:
-        x,y = pyautogui.position()
+        x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 except KeyboardInterrupt:
     print('\n')
-
