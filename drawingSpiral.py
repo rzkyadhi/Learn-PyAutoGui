44d091b144aeb97ed6a2c58056b02eb858497acd
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-11-06 13:49:07 +0700

    Making file drawingSpiral.py
    This Python File use to making rectangle spiral on specific x and y

---
+++ drawingSpiral.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+time.sleep(1)
+distance = 300
+
+pyautogui.moveTo(750, 300)
+
+while distance>0:
+    pyautogui.dragRel(distance, 0, 1, button="left")
+    distance = distance - 20
+    pyautogui.dragRel(0, distance, 1, button="left")
+    pyautogui.dragRel(-distance, 0, 1, button="left")
+    distance = distance - 20
+    pyautogui.dragRel(0, -distance, 1, button="left")
+
